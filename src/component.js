baf09871397c20b42905fe8480e053d12b79908a
tesfayeA <tesfayeabera@gmail.com>
2026-04-12 03:09:18 +0100

    adding the button panels

---
+++ src/component.js
@@ -0,0 +1,20 @@
+ function Profile() {
+  return (
+    <img
+      src="https://react.dev/images/docs/scientists/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
